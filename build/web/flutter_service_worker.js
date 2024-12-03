8fe71feea223a725d7727f9855a36defa5c4d21c
Jang So Young <theplaceyoung@gmail.com>
2024-12-03 23:38:22 +0900

    Fix base href

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -22,14 +22,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "7c73ccc4bf94a561e113463d63e0c6ac
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "3b2e7f6d404e168275fbcb901a54611a",
+"flutter_bootstrap.js": "20c794b8ab9993281d551d71b7a99046",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "d4069e394894b95a64e0de3496371f5b",
 "/": "d4069e394894b95a64e0de3496371f5b",
-"main.dart.js": "4d680d220ecf1543e92034325d82b081",
+"main.dart.js": "32262afe345d8008a38e8f99e9692927",
 "manifest.json": "d6ca57350173b3ccd7074f958253fbee",
 "version.json": "c8f979d408e51acd1ee06f6af9b9722c"};
 // The application shell files that are downloaded before a service worker can
