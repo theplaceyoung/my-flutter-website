96e1377033c0729f695c0723764377722e4a560b
Jang So Young <theplaceyoung@gmail.com>
2024-12-03 23:51:14 +0900

    add built web files

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1458315070"
+    serviceWorkerVersion: "2991224686"
   }
 });
